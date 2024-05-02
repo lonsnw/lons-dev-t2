5552480462222b96671b678ca16b3a3788ba68c7
lons <lonsnw@gmail.com>
2024-05-02 10:22:04 -0500

    Updating links

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <h2>This website is under construction</h2>
       <nav >
         <ul>
-          <li><a href="/index.html">Home</a></li>
-          <li><a href="/about.html">Resume</a></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="https://lons.dev/about.html">Resume</a></li>
           <li><a href="https://www.linkedin.com/in/lonsnw/">LinkedIn</a></li>
           <li><a href="https://github.com/lonsnw">GitHub</a></li>
         </ul>
